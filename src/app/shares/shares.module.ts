cec745f740bfffd8fd57a3797f68225e0b5b4eba
mayuhome <mayuhome@163.com>
2019-12-03 23:14:26 +0800

    添加playcount管道

--- src/app/shares/shares.module.ts
+++ src/app/shares/shares.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { FormsModule } from '@angular/forms';
 import { WyUiModule } from './wy-ui/wy-ui.module';
+import { PlayCountPipe } from './play-count.pipe';
 
 
 
