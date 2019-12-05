d359623883551fe1af9ca2fded001cf8689e1c14
mayuhome <mayuhome@163.com>
2019-12-05 23:59:23 +0800

    底部播放条

--- src/app/shares/shares.module.ts
+++ src/app/shares/shares.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { FormsModule } from '@angular/forms';
 import { WyUiModule } from './wy-ui/wy-ui.module';
-import { PlayCountPipe } from './play-count.pipe';
-
 
 
 @NgModule({
@@ -13,13 +11,15 @@ import { PlayCountPipe } from './play-count.pipe';
     CommonModule,
     NgZorroAntdModule,
     FormsModule,
-    WyUiModule
+    WyUiModule,
+    
   ],
   exports: [
     CommonModule,
     NgZorroAntdModule,
     FormsModule,
-    WyUiModule
+    WyUiModule,
+    
   ]
 })
 export class SharesModule { }
